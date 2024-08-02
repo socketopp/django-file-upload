4260bf383dedbd5622450c1dcd8e3a5b20e28a50
socketopp <sokrates.a.lamprou@gmail.com>
2024-08-02 20:47:18 +0200

    Remove unused stuff

--- svelte-app/vite.config.ts
+++ svelte-app/vite.config.ts
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { type ViteDevServer, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
